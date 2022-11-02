a2ad672c9d8f30439733de89bdd5b18755940185
PulyaPolya <polinaitsme@gmail.com>
2022-11-02 16:15:42 +0100

    decryption and encryption works fine

--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,24 @@
 import random
 import math
+def bitfield(n):
+    return [1 if digit=='1' else 0 for digit in bin(n)[2:]]
+def bit_to_byte(sequence):
+    n = len(sequence)
+    resulting_len = math.floor(n/8)
+    bait_arr = []
+    rest = n %8
+    for i in range(0,n - rest, 8):
+        bin = sequence[i: i+8]
+        res = int("".join(str(x) for x in bin), 2)
+        bait_arr.append(res)
+    if rest > 0:
+        residue = sequence[-rest:]
+        arr = [0]*(8-rest)
+        arr += residue
+        res = int("".join(str(x) for x in arr), 2)
+        bait_arr.append(res)
+    return bait_arr
+
 def generate_number(range):
     r = random.randint(range[0], range[1])
     return r
@@ -38,8 +57,8 @@ def generate_prime(n0, n1, k=100):
     for i in range(k):
         x = generate_number([n0, n1])
         m0 = x if x % 2 == 1 else x + 1
-        for i in range(int((n1 - m0)/2)):
-            p = m0+ 2*i
+        for j in range(int((n1 - m0)/2)):
+            p = m0+ 2*j
             if miller_rabin(p) == 'prime':
                 return p
 
@@ -62,11 +81,10 @@ def generate_keys(n0,n1):
     q = generate_p(n0, n1)
     n = p*q
     phi = count_phi(p,q)
-    #e = generate_number([2, phi])
     e = 2**16+1
     d  = pow(e, -1, phi)
-    secret = (d, p, q)
-    public = (n,e)
+    secret = {'d':d, 'p':p, 'q':q}
+    public = {'n' :n,'e' :e}
     return secret, public
 
 def cypher(m, e, n):
@@ -83,5 +101,21 @@ def verify(m,s, e,n):
         return 'correct'
     else:
         return 'wrong'
+m = 1234
 
-# print(generate_keys(1,100))
+secret, public = generate_keys(1,1000)
+print(secret, public)
+n = public["n"]
+e = public["e"]
+d = secret['d']
+print(f'n: {n}, e: {e}, m: {m}')
+print(f'n_hex: {hex(n)[2:]}, e_hex: {hex(e)[2:]}, m_hex: {hex(m)[2:]}')
+# cypher = cypher(m,e,n)
+# print(hex(cypher)[2:])
+# message = decode(cypher, d,n)
+# print(message)
+public_exp = 0x10001
+modulus= 0x87DE232DF952027CE97F6649F3FC1B43
+code = cypher(m, public_exp,modulus)
+print(hex(code))
+
